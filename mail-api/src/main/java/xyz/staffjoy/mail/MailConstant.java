e7c95b959311f86a7b40572c739960accb47e903
Leon <mail@omgxy.com>
2022-01-15 22:50:17 +0800

    chore: evaluation, change some config

--- mail-api/src/main/java/xyz/staffjoy/mail/MailConstant.java
+++ mail-api/src/main/java/xyz/staffjoy/mail/MailConstant.java
@@ -4,7 +4,7 @@ public class MailConstant {
     public static final String SERVICE_NAME = "email-service";
 
     public static final String FROM_NAME = "Staffjoy";
-    public static final String FROM = "staffjoy@dm.staffjoy.xyz";
-    public static final String STAFFJOY_EMAIL_SUFFIX = "@jskillcloud.com";
+    public static final String FROM = "noreply@dm.omgxy.com";
+    public static final String STAFFJOY_EMAIL_SUFFIX = "@omgxy.com";
     public static final String ALIYUN_REGION_ID = "cn-hangzhou";
 }
